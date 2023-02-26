fc2c90885dbde7c3caec85adb2eb6fe5710e0d08
pro-tyush <pratyushedu2510@gmail.com>
2023-02-26 14:56:31 +0530

    added missing server files, fixed index.js

--- node-http-server/index.js
+++ node-http-server/index.js
@@ -1,51 +1,43 @@
-// Run this using node to start directory http server
-const express = require('express');
+const express = require('express')
 const fs = require('fs');
 const path = require('path');
+const app = express()
+const port = 3000
 
-const app = express();
-const port = 3000;
+const ROOT_DIR = "/";
 
-const ROOT_DIR = '/';
-
-const fsExplore = async (req, res) => {
-    console.log('HIT > !');
+const fsExplore = (req,res) => {
+    console.log("HIT > !")
     // Get path
-    let dirPath = req.path;
-    dirPath = path.join(ROOT_DIR, dirPath);
-    // Remove file extension
-    dirPath = path.parse(dirPath).dir + '/' + path.parse(dirPath).name;
+    const dirPath  = req.path;
+    dirPath.replace(".","");
 
-    try {
-        const stat = await fs.promises.stat(dirPath);
-        if (stat.isFile()) {
-            const content = await fs.promises.readFile(dirPath, 'utf8');
-            return res.send(content);
-        } else if (stat.isDirectory()) {
-            const files = await fs.promises.readdir(dirPath);
-            const resArr = files
-                .filter(file => !file.startsWith('.'))
-                .map(file => {
-                    const filePath = path.join(dirPath, file);
-                    const fileStat = fs.statSync(filePath);
-                    return {
-                        name: file,
-                        dir: fileStat.isDirectory(),
-                        ref: path.join(req.path, file),
-                    };
-                });
-            res.json(resArr);
-        } else {
-            throw new Error(`Invalid path: ${dirPath}`);
+    // Add root path
+    const fullDirPath =  path.join(ROOT_DIR, dirPath);
+
+    // return content of file if not dir
+    if(!fs.statSync(fullDirPath).isDirectory())
+        return res.send(fs.readFileSync(fullDirPath, 'utf8'));
+
+    // Generate details for each file / dir in current dir
+    const resArr = [];
+    fs.readdirSync(fullDirPath).forEach(
+        file =>
+        {
+            if(file[0] != ".")
+                resArr.push({
+                            "name" : file,
+                            "dir" : fs.statSync(path.join(fullDirPath,file)).isDirectory(),
+                            "ref" : path.join(fullDirPath,file)
+                    })
         }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-};
+    );
 
-app.get('*', fsExplore);
+    // Send array
+    res.json(resArr);
+}
 
+app.get("*",fsExplore);
 app.listen(port, () => {
-    console.log(`Dir app listening on port ${port}`);
-});
+    console.log(`Dir app listening on port ${port}`)
+})
